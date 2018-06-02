7cb6dd4cf9bd0b1c32eb394e3e4f550cb86352a7
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-06-02 02:10:24 +0200

    Update default skin & css structure

---
+++ public/js/panelController.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const panel = document.getElementById("panelController");
+    const leftPanel = document.getElementById("leftPanel");
+    let isClose = false;
+
+    panel.addEventListener("mouseover", () => {
+        panel.classList.add(isClose ? "icon-right-dir" : "icon-left-dir");
+    });
+
+    panel.addEventListener("mouseleave", () => {
+        panel.classList.add(isClose ? "icon-right-dir" : "icon-left-dir");
+    });
+
+    panel.addEventListener("click", () => {
+        if(isClose === true){
+            panel.classList.remove("icon-right-dir");
+            leftPanel.style.display = "flex";
+        }
+        else{
+            panel.classList.remove("icon-left-dir");
+            leftPanel.style.display = "none";
+        }
+        isClose = !isClose;
+    });
+});
